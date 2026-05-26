f181ead1bf5ab16474bec6c848d9ee63ba415947
Julian <julixnj31@gmail.com>
2026-05-26 00:29:34 -0500

    feat(create): agregar registro de tareas mediante peticiones POST

--- cliente/app.js
+++ cliente/app.js
@@ -49,3 +49,82 @@ async function getTasks() {
 // =========================================
 
 getTasks();
+
+// =========================================
+// ISSUE #3
+// CREAR TAREA
+// =========================================
+//
+// Esta funcion:
+//
+// ✔ captura formulario
+// ✔ valida input
+// ✔ envia POST
+// ✔ actualiza DOM
+//
+// =========================================
+
+async function createTask(event) {
+
+  // Evitar recarga
+  event.preventDefault();
+
+  // Obtener valor input
+  const title =
+    taskTitle.value.trim();
+
+  // Validar vacio
+  if (title === "") {
+
+    message.textContent =
+      "La tarea es obligatoria";
+
+    return;
+  }
+
+  // Crear objeto
+  const newTask = {
+
+    title: title
+
+  };
+
+  // Enviar POST
+  await fetch(URL, {
+
+    method: "POST",
+
+    headers: {
+
+      "Content-Type":
+        "application/json"
+
+    },
+
+    body:
+      JSON.stringify(newTask)
+
+  });
+
+  // Mensaje exito
+  message.textContent =
+    "Tarea creada";
+
+  // Limpiar input
+  taskTitle.value = "";
+
+  // Actualizar lista
+  getTasks();
+
+}
+
+
+
+// =========================================
+// EVENTO FORMULARIO
+// =========================================
+
+taskForm.addEventListener(
+  "submit",
+  createTask
+);
